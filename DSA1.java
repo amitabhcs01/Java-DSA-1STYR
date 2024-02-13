67b70abbbf84f774b6db6e46890f89045e1bd019
Amitabh <amitabh1ranjan@gmail.com>
2024-02-13 13:58:57 +0530

    changes1

--- DSA1.java
+++ DSA1.java
@@ -4,6 +4,6 @@
 public class DSA1 {
 
     public static void main(String[] args) {
-        System.out.println("Amitabh Ranjan is a good boy");
+        System.out.println("Helllo Java");
     }
 }
