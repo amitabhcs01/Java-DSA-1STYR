6b844baecf4b8e3b5f06ecc67c6f3ac82ddfc8ff
Amitabh <amitabh1ranjan@gmail.com>
2024-02-22 09:11:20 +0530

    changes have made in solving add eqn

--- hello.java
+++ hello.java
@@ -1,9 +1,12 @@
 /**
- * DSA1
+ * hello
  */
 public class hello {
 
     public static void main(String[] args) {
-        System.out.println("Helllo Java");
+    int a = 2;
+    int b = 333;
+    int c = a+b;
+    System.err.println(c);
     }
 }
